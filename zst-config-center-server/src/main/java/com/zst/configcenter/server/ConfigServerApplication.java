d7a567c38397ebfcbbc0d368c83b723d3ada76b9
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-05-04 09:50:12 +0800

    style: 加个checkstyle

--- zst-config-center-server/src/main/java/com/zst/configcenter/server/ConfigServerApplication.java
+++ zst-config-center-server/src/main/java/com/zst/configcenter/server/ConfigServerApplication.java
@@ -1,6 +1,5 @@
 package com.zst.configcenter.server;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- zst-config-center-server/src/main/java/com/zst/configcenter/server/module/config/Config.java
+++ zst-config-center-server/src/main/java/com/zst/configcenter/server/module/config/Config.java
@@ -1,7 +1,6 @@
 package com.zst.configcenter.server.module.config;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import jakarta.validation.constraints.NotEmpty;
